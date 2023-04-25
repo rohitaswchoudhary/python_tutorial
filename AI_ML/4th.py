174fe4728da64f1a8a4837aa0ff7066b3a1c53df
rohitaswchoudhary <rohitaswchoudhary@gmail.com>
2023-04-25 18:33:55 +0530

    25

--- AI_ML/4th.py
+++ AI_ML/4th.py
@@ -1,7 +1,10 @@
 # power table of any number using anonymous function
 # 2023-04-25 17:42:06
-term = int(input("enter the number of terms: "))
-n = int(input("enter the number to find the power table:"))
+# term = int(input("enter the number of terms: "))
+# n = int(input("enter the number to find the power table:"))
+
+term = 5
+n = 5
 
 result = list(map(lambda x: n**x, range(term)))
 print(result)
@@ -9,4 +12,21 @@ print(result)
 for i in range(term):
     print(f"{n}^{i} is", result[i])
 
-    
+num = range(1,term,1)
+print(num)
+print(type(num))
+
+sentence = "A quick brown fox jumps over the lazy dog.       "
+number = "5"
+print(sentence.upper())
+print(sentence.lower())
+print(sentence.partition("jumps"))
+print(sentence.find("z"))
+print(sentence, sentence)
+print(sentence.rstrip(),sentence)
+print(sentence.split("o"))
+print(number.isnumeric())
+print(sentence.isnumeric())
+
+
+
---
+++ AI_ML/5th.py
@@ -0,0 +1,7 @@
+numbers = [1,2,3,4,5]
+result = [n for n in numbers if n%2==0]
+print(result)
+
+string = "a quick brown fox jumps over the lazy dog"
+
+print()
